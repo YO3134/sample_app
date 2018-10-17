85e9eae933064760379170cde265eb1ec81db7e3
YO3134 <nishiura.145@gmail.com>
2018-10-17 22:58:31 +0900

    Add views about home help files

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
   # [Homeページのテスト。GETリクエストをhomeアクションに対して発行せよ。そうすれば、リクエストに対するレスポンスは成功になるはず。]
 
@@ -11,10 +12,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
