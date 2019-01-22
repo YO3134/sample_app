638699c92aaa5c5130f630c902eb28e203039ce0
YO3134 <nishiura.145@gmail.com>
2019-01-22 21:52:54 +0900

    Setting New password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
 root 'static_pages#home'
@@ -25,4 +29,6 @@ root 'static_pages#home'
 
   resources :users
   resources :account_activations, only:[:edit]
+  resources :password_resets, only:[:new, :create, :edit, :update]
+  # resourcesを用いる
 end
